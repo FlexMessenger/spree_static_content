4a0759d01222c7efed6536c8c3f34db25ee908c0
Edward Yang <yushine999@gmail.com>
2015-07-21 10:09:29 +0800

    Conflict with LH model StaticPage

--- lib/spree_static_content.rb
+++ lib/spree_static_content.rb
@@ -3,7 +3,7 @@ require 'spree_static_content/engine'
 require 'coffee_script'
 require 'sass/rails'
 
-module StaticPage
+module SpreeStaticPage
   def self.remove_spree_mount_point(path)
     regex = Regexp.new '\A' + Rails.application.routes.url_helpers.spree_path
     path.sub( regex, '').split('?')[0]
